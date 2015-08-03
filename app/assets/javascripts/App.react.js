7c0c284e65019ed7ce476ea3fb8d48d2f8e6c3f3
Michelle Hood <meesh121@gmail.com>
2015-08-03 16:20:11 -0400

    react added

---
+++ app/assets/javascripts/App.react.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Bonjour!</h1>
+      </div>
+    )
+  }
+
+
+});
+
+module.exports = App;
