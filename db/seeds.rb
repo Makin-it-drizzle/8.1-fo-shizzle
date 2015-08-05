283c8d780aaed1c336b43f4710b87e82fe01d43d
Sean Page <seanpage59@gmail.com>
2015-08-04 22:01:35 -0400

    have the create on server side up

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+UserInfo.create!([
+  {name: 'Sean', address: '123 address', phone: '555-555-5555', email: 'sean@dollaz.com', password_digest: 'password'},
+  {name: 'Meeeesh', address: '234 address', phone: '111-111-1111', email: 'meeesh@dollaz.com', password_digest: 'password'},
+  {name: 'Tibby', address: '345 address', phone: '800-123-RUFF', email: 'tibby@dollaz.com', password_digest: 'password'}
+])
