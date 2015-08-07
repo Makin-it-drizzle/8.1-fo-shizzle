ab75cf3c59e95e2d1d131a6dc15e46acad466fa7
Sean Page <seanpage59@gmail.com>
2015-08-07 19:39:15 -0400

    fixed issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @user_customer ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
   helper_method :current_user
+
 end
