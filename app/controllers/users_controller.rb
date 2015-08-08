763969d30da9a2bbad3b70f3a9adfbe999299929
Sean Page <seanpage59@gmail.com>
2015-08-08 15:24:32 -0400

    fixed conclicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    if current_user
+      @user = get_user
+    else
+      redirect_to root_path
+    end
+  end
+
 
   private
 
   def user_params
     params.require(:user).permit(:name, :address, :phone, :email, :password, :password_confirmation)
+  end
+
+  def get_user
+    binding.pry
+    User.find(session[:user_id])
   end
 
 
