070f446cc84694790cd7061deef5bcda2cde3de6
Sean Page <seanpage59@gmail.com>
2015-08-13 08:46:51 -0400

    set the heroku aws stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,18 +6,4 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
 
-  if Rails.env.test?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-
-  elsif Rails.env.development?
-    config.storage = :file
-  else
-    config.storage = :fog
-  end
-  
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_directory  = 'foshizzle-avatar'
-
 end
